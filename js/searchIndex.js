87a31189c11a8cb3a79f4bda2e8a34d5ca169751
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2021-08-07 01:02:38 +0000

    Updating documentation to version 1.0.0

---
+++ js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\LaunchDarkly\\Integrations\\Consul",
+            "name": "Consul",
+            "summary": "Integration\u0020with\u0020a\u0020Consul\u0020data\u0020store.",
+            "url": "classes/LaunchDarkly-Integrations-Consul.html"
+        },                {
+            "fqsen": "\\LaunchDarkly\\Integrations\\Consul\u003A\u003AfeatureRequester\u0028\u0029",
+            "name": "featureRequester",
+            "summary": "Configures\u0020an\u0020adapter\u0020for\u0020reading\u0020feature\u0020flag\u0020data\u0020from\u0020Consul.",
+            "url": "classes/LaunchDarkly-Integrations-Consul.html#method_featureRequester"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\LaunchDarkly\\Integrations",
+            "name": "Integrations",
+            "summary": "",
+            "url": "namespaces/launchdarkly-integrations.html"
+        },                {
+            "fqsen": "\\LaunchDarkly",
+            "name": "LaunchDarkly",
+            "summary": "",
+            "url": "namespaces/launchdarkly.html"
+        }            ]
+);
